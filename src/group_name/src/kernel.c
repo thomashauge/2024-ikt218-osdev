148737253b8a933ef64a9b11d02aa71d144815d4
thomashauge <130986710+thomashauge@users.noreply.github.com>
2024-05-31 00:10:32 +0200

    Update kernel.c

--- src/group_name/src/kernel.c
+++ src/group_name/src/kernel.c
@@ -1,8 +1,9 @@
-#include "libc/stdint.h"
-#include "libc/stddef.h"
-#include "libc/stdbool.h"
-
+#include <stdint.h>
+#include <stddef.h>
+#include <stdbool.h>
 #include <multiboot2.h>
+
+
 
 struct multiboot_info {
     uint32_t size;
@@ -12,7 +13,10 @@ struct multiboot_info {
 
 int kernel_main();
 
+
 int main(uint32_t magic, struct multiboot_info* mb_info_addr) {
+
+
     // Call cpp kernel_main (defined in kernel.cpp)
     return kernel_main();
 }
